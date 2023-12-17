98ffcdc103ccba7fddf477059e34e0dd9114c155
dustij <knightlybyte@gmail.com>
2023-12-17 05:33:40 -0700

    Setup folder structure

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+interface Props {}
+
+const LoginPage: NextPage<Props> = ({}) => {
+  return <div>Login Page (Unprotected)</div>
+}
+
+export default LoginPage
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+interface Props {}
+
+const RootPage: NextPage<Props> = ({}) => {
+  return <div>Root Page (Protected)</div>
+}
+
+export default RootPage
