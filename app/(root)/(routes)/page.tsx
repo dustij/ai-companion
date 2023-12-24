e2ff9e015ccd905a1fe9e5911df66098ba112f73
dustij <knightlybyte@gmail.com>
2023-12-24 05:25:05 -0700

    Added message count

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from "next"
 
-import { and, eq, sql } from "drizzle-orm"
+import { and, count, eq, sql } from "drizzle-orm"
 
 import Categories from "~/components/categories"
 import SearchInput from "~/components/search-input"
 import { db } from "~/db"
-import { category, companion } from "~/db/schema"
+import { category, companion, message } from "~/db/schema"
 
 interface Props {
   searchParams: {
@@ -18,22 +18,67 @@ const RootPage: NextPage<Props> = async ({ searchParams }) => {
   let data
   if (searchParams && searchParams.categoryId && !searchParams.name) {
     data = await db
-      .select()
+      .select({
+        id: companion.id,
+        userId: companion.userId,
+        userName: companion.userName,
+        src: companion.src,
+        name: companion.name,
+        description: companion.description,
+        instructions: companion.instructions,
+        seed: companion.seed,
+        createdAt: companion.createdAt,
+        updatedAt: companion.updatedAt,
+        categoryId: companion.categoryId,
+        messageCount: count(message.id),
+      })
       .from(companion)
+      .leftJoin(message, eq(companion.id, message.companionId))
+      .groupBy(companion.id)
       .where(eq(companion.categoryId, searchParams.categoryId))
       .orderBy(companion.createdAt)
   } else if (searchParams && searchParams.name && !searchParams.categoryId) {
     data = await db
-      .select()
+      .select({
+        id: companion.id,
+        userId: companion.userId,
+        userName: companion.userName,
+        src: companion.src,
+        name: companion.name,
+        description: companion.description,
+        instructions: companion.instructions,
+        seed: companion.seed,
+        createdAt: companion.createdAt,
+        updatedAt: companion.updatedAt,
+        categoryId: companion.categoryId,
+        messageCount: count(message.id),
+      })
       .from(companion)
+      .leftJoin(message, eq(companion.id, message.companionId))
+      .groupBy(companion.id)
       .where(
         sql`MATCH (name) AGAINST (${searchParams.name} IN NATURAL LANGUAGE MODE)`,
       )
       .orderBy(companion.createdAt)
   } else if (searchParams && searchParams.name && searchParams.categoryId) {
     data = await db
-      .select()
+      .select({
+        id: companion.id,
+        userId: companion.userId,
+        userName: companion.userName,
+        src: companion.src,
+        name: companion.name,
+        description: companion.description,
+        instructions: companion.instructions,
+        seed: companion.seed,
+        createdAt: companion.createdAt,
+        updatedAt: companion.updatedAt,
+        categoryId: companion.categoryId,
+        messageCount: count(message.id),
+      })
       .from(companion)
+      .leftJoin(message, eq(companion.id, message.companionId))
+      .groupBy(companion.id)
       .where(
         and(
           sql`MATCH (name) AGAINST (${searchParams.name} IN NATURAL LANGUAGE MODE)`,
@@ -42,8 +87,28 @@ const RootPage: NextPage<Props> = async ({ searchParams }) => {
       )
       .orderBy(companion.createdAt)
   } else {
-    data = await db.select().from(companion).orderBy(companion.createdAt)
+    data = await db
+      .select({
+        id: companion.id,
+        userId: companion.userId,
+        userName: companion.userName,
+        src: companion.src,
+        name: companion.name,
+        description: companion.description,
+        instructions: companion.instructions,
+        seed: companion.seed,
+        createdAt: companion.createdAt,
+        updatedAt: companion.updatedAt,
+        categoryId: companion.categoryId,
+        messageCount: count(message.id),
+      })
+      .from(companion)
+      .leftJoin(message, eq(companion.id, message.companionId))
+      .groupBy(companion.id)
+      .orderBy(companion.createdAt)
   }
+
+  console.log(data)
 
   const categories = await db.select().from(category)
 
